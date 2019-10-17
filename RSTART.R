65451c8c41514315e782c86a416dd00ec3b9f80f
LUCIE ARACIL <lucie.aracil@outlook.fr>
2019-10-17 11:16:52 +0200

    start2

--- RSTART.R
+++ RSTART.R
@@ -1,1 +1,9 @@
-# TEST START
+# TEST START    
+
+# le X s'est rajouté tout seul en important les datas
+head(X1_activityrate)
+head(X2_employmentrate)
+head(X3_parttime)
+head(X4_temporary)
+head(X5_unemplyment)
+head(X6_LTunemployment)
